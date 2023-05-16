83821c0cd9296dbdefa89e1d787f46537de79f21
Minepluie <126269059+Minepluie@users.noreply.github.com>
2023-05-16 11:22:00 +0000

    0.0

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import tkinter
-import smtp
-import mysql-connector as MC
+from tkinter import *
+import mysql.connector as MC
+import game
+import smtplib
+window = Tk()
+window.mainloop()
+window.title("Pas d’idee de nom")
+window.geometry("480x360")
+
+print("Le programme a bien été lancé")
+# connect = MC.
