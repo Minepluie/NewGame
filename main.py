02f932d8616f5a9c62ad63253726b0a0fc093b43
Minepluie <126269059+Minepluie@users.noreply.github.com>
2023-05-16 11:28:08 +0000

    0.0.0

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ window = Tk()
 window.mainloop()
 window.title("Pas d’idee de nom")
 window.geometry("480x360")
+window.minsize(480, 360)
+window.maxsize(1080, 720)
 
 print("Le programme a bien été lancé")
 # connect = MC.
