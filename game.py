195f13c15f43bc797c72609a90d85343f3b301d9
Minepluie <126269059+Minepluie@users.noreply.github.com>
2023-05-16 11:47:37 +0000

    0.0

--- game.py
+++ game.py
@@ -3,3 +3,12 @@ pygame.init()
 
 pygame.display.set_mode((800, 600))
 pygame.display.set_caption("New Game Start")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+pygame.quit()
